d11bb60141e6f3e375633f8cb34b92c2d1fb8500
JSzyper <jacek.szyper@savangard.com>
2021-02-18 17:01:41 +0100

    Added Ingredient, Notes, Recipe, UnitOfMeasure entity

---
+++ src/main/java/domain/Notes.java
@@ -0,0 +1,33 @@
+package domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // w tym wypadku Recipe jest właścicielem Notes. Niechcemy gdy usuwa się Notes też usuwało Recipe. dlatego tu nie dodajemy kaskady.
+    @OneToOne
+    private Recipe recipe;
+    @Lob
+    private String recipeNotes;
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
