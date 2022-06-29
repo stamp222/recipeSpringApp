57aae562a03e116d8c477a58cae2d90c86c4066a
stamp222 <jacekszyper@gmail.com>
2022-06-29 10:58:36 +0200

    configure dev and prod profile for mysql

--- src/main/scripts/configure-mysql.sql
+++ src/main/scripts/configure-mysql.sql
@@ -2,17 +2,34 @@
 
 # connect to my sql and run as a root user
 
+## Use to run mysql db docker image, optional if you're not using a local mysqldb
+# docker run --name mysqldb -p 3306:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -d mysql
+
+# connect to mysql and run as root user
+#Create Databases
 CREATE DATABASE stamp_dev;
 CREATE DATABASE stamp_prod;
 
-CREATE USER 'stamp_dev_user'@'locahost' IDENTIFIED BY 'stamp';
-CREATE USER 'stamp_prod_user'@'locahost' IDENTIFIED BY 'stamp';
+#Create database service accounts
+CREATE USER 'stamp_dev_user'@'localhost' IDENTIFIED BY 'stamp';
+CREATE USER 'stamp_prod_user'@'localhost' IDENTIFIED BY 'stamp';
+CREATE USER 'stamp_dev_user'@'%' IDENTIFIED BY 'stamp';
+CREATE USER 'stamp_prod_user'@'%' IDENTIFIED BY 'stamp';
 
-GRANT SELECT ON stamp_dev.* to 'stamp_dev_user'@'locahost';
-GRANT INSERT ON stamp_dev.* to 'stamp_dev_user'@'locahost';
-GRANT DELETE ON stamp_dev.* to 'stamp_dev_user'@'locahost';
-GRANT UPDATE ON stamp_dev.* to 'stamp_dev_user'@'locahost';
-GRANT SELECT ON stamp_prod.* to 'stamp_prod_user'@'locahost';
-GRANT INSERT ON stamp_prod.* to 'stamp_prod_user'@'locahost';
-GRANT DELETE ON stamp_prod.* to 'stamp_prod_user'@'locahost';
-GRANT UPDATE ON stamp_prod.* to 'stamp_prod_user'@'locahost';
+#Database grants
+GRANT SELECT ON stamp_dev.* to 'stamp_dev_user'@'localhost';
+GRANT INSERT ON stamp_dev.* to 'stamp_dev_user'@'localhost';
+GRANT DELETE ON stamp_dev.* to 'stamp_dev_user'@'localhost';
+GRANT UPDATE ON stamp_dev.* to 'stamp_dev_user'@'localhost';
+GRANT SELECT ON stamp_prod.* to 'stamp_prod_user'@'localhost';
+GRANT INSERT ON stamp_prod.* to 'stamp_prod_user'@'localhost';
+GRANT DELETE ON stamp_prod.* to 'stamp_prod_user'@'localhost';
+GRANT UPDATE ON stamp_prod.* to 'stamp_prod_user'@'localhost';
+GRANT SELECT ON stamp_dev.* to 'stamp_dev_user'@'%';
+GRANT INSERT ON stamp_dev.* to 'stamp_dev_user'@'%';
+GRANT DELETE ON stamp_dev.* to 'stamp_dev_user'@'%';
+GRANT UPDATE ON stamp_dev.* to 'stamp_dev_user'@'%';
+GRANT SELECT ON stamp_prod.* to 'stamp_prod_user'@'%';
+GRANT INSERT ON stamp_prod.* to 'stamp_prod_user'@'%';
+GRANT DELETE ON stamp_prod.* to 'stamp_prod_user'@'%';
+GRANT UPDATE ON stamp_prod.* to 'stamp_prod_user'@'%';
